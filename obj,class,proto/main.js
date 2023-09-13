dfc6b6c0923e18ad11c7b4091ed606744a6d7568
Jorge Suárez <suarezjorge758@gmail.com>
2023-09-13 11:04:37 -0400

    a

---
+++ obj,class,proto/main.js
@@ -0,0 +1,14 @@
+const sthephany = {
+    nombre: 'Sthephany',
+    apellido: 'Fernandez',
+    edad: 19,
+    cursosAprobados: [
+        'Audiocurso glosario de términos de diseño',
+        'Curso de uso y manejo del color',
+        'Curso básico de Photoshop',
+    ],
+};
+
+// sthephany aprueba otro curso
+
+sthephany.cursosAprobados.push('Curso de Diseño para Redes Sociales');
