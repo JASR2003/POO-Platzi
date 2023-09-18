35bf4c6f790582769a397cfc341ce4a6a4d5b4a4
Jorge Suárez <suarezjorge758@gmail.com>
2023-09-18 12:33:09 -0400

    instancias

--- obj,class,proto/literalesVsInsancias.js
+++ obj,class,proto/literalesVsInsancias.js
@@ -59,3 +59,62 @@ const jose1 = {
 // al ser objetos literales hay que crear uno por vez aunque se repitan muchos los elementos, por cada uno son 30 lineas aproximadamente, son son 10 alumnos son 300 lineas solo en la creación de alumnos
 
 // otra alternativa son las instancias, donde se construye una especie de plantilla, y se usan 40 para la plantilla, pero 3 para cada alumno y a largo plazo termina siendo más rentable
+
+class Student {
+    constructor({
+        name,
+        email,
+        username,
+        instagram = undefined,
+        twitter = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = []
+    }) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            instagram,
+            twitter,
+            facebook
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+class LearningPath {
+    constructor ({
+        name,
+        teacher,
+        courses = [],
+    }){
+        this.name = name;
+        this.teacher = teacher;
+        this.courses = courses;
+    }
+}
+
+const escuelaWeb = new LearningPath({
+    name: 'escuela web',
+    teacher: 'Diego de Granda',
+    courses: [
+        'curso definitivo de HTML y CSS',
+        'curso practico de HTML y CSS',
+        'curso de responsive design',
+    ]
+})
+
+const juan2 = new Student({
+    name: 'Juan',
+    username: 'JuanDC',
+    email: 'juan123@gmail.com',
+    instagram: 'fjuandc'
+})
+const jose2 = new Student({
+    name: 'Jose',
+    username: 'joseED',
+    email: 'jose123@gmail.com',
+    twitter: 'fjoseED'
+})
