d2a9f624590621804374a4f8a620e78e9bdeaf45
Jorge Suárez <suarezjorge758@gmail.com>
2023-09-18 09:33:04 -0400

    literales

---
+++ obj,class,proto/literalesVsInsancias.js
@@ -0,0 +1,61 @@
+// Objetos literales
+const juan1 = {
+    nombre: 'Juan',
+    nombreUsuario: 'JuanDC',
+    puntos: 100,
+    socialMedia: {
+        twitter: '@fjuandc',
+        instagram: 'fjuandc',
+        facebook: null,
+    },
+    rutasAprendizaje: [
+        {
+            nombre: 'Desarrollo web',
+            cursos: [
+            'curso definitivo de HTML y CSS',
+            'curso practico de HTML y CSS',
+            'curso de responsive design',
+            ],
+        },
+        {
+            nombre: 'Escuela de Videojuegos',
+            cursos: [
+            'curso introducción a la producción de Videojuegos',
+            'curso Unreal Engine',
+            'curso de Unity 3D',
+            ],
+        },
+    ],
+};
+
+const jose1 = {
+    nombre: 'José',
+    nombreUsuario: 'JoseED',
+    puntos: 1924,
+    socialMedia: {
+        twitter: '@joseED',
+        instagram: 'joseED',
+        facebook: null,
+    },
+    rutasAprendizaje: [
+        {
+            nombre: 'Desarrollo web',
+            cursos: [
+            'curso definitivo de HTML y CSS',
+            'curso practico de HTML y CSS',
+            'curso de responsive design',
+            ],
+        },
+        {
+            nombre: 'Escuela de Videojuegos',
+            cursos: [
+            'curso introducción a la producción de Videojuegos',
+            'curso Unreal Engine',
+            'curso de Unity 3D',
+            ],
+        },
+    ],
+};
+// al ser objetos literales hay que crear uno por vez aunque se repitan muchos los elementos, por cada uno son 30 lineas aproximadamente, son son 10 alumnos son 300 lineas solo en la creación de alumnos
+
+// otra alternativa son las instancias, donde se construye una especie de plantilla, y se usan 40 para la plantilla, pero 3 para cada alumno y a largo plazo termina siendo más rentable
